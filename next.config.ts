ac6488d1394b2bbf6642b5e48ac3e3685d81ce6f
MHA <98241106+MHA963@users.noreply.github.com>
2025-02-18 23:35:47 +0100

    adding gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // Export static HTML
+  images: { unoptimized: true }, // Disable image optimization for static export
+  basePath: "/Sy-Net", // Change this to match your repo name
+  assetPrefix: "/Sy-Net",
 };
 
 export default nextConfig;
